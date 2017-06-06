6fbb12b5688c5bbee2903ac3a692328d497a0ddf
Xiaoyin Liu <xiaoyinl@users.noreply.github.com>
2017-06-06 13:11:58 -0400

    remove header RuntimePlatformAgnosticPch.h from SystemInfo.Common.cpp

--- lib/Runtime/PlatformAgnostic/Platform/Common/SystemInfo.Common.cpp
+++ lib/Runtime/PlatformAgnostic/Platform/Common/SystemInfo.Common.cpp
@@ -3,7 +3,6 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 
-#include "RuntimePlatformAgnosticPch.h"
 #include "Common.h"
 #include "ChakraPlatform.h"
 #if defined(__APPLE__)
